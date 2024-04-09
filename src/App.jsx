e2e7130304ac29d205b3e0413d5ade66e60bb04d
SameerAhmed1402 <sameerkuraishi1402@gmail.com>
2024-04-09 12:50:48 +0530

    final submit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MovieBooking from './Components/MovieBooking'
+import "../src/App.css"
+
+const App = () => {
+  return (
+    <div className='App'>
+    <MovieBooking />
+    </div>
+  )
+}
+
+export default App
